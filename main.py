ef099c79029a73d0c2e790cc366ccab134579c62
Ilya Kanishev <kanishev2002@gmail.com>
2021-12-25 22:20:07 +0300

    Implement random meme functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """A telegram bot that sends a random meme to the user."""
 from os import environ as env
 
+import requests
 import telebot
 
 TOKEN = env.get('TOKEN')
@@ -8,10 +9,20 @@ print(f'Token is: {TOKEN}')
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler()
-def echo_all(message):
-    """Handle a message from user."""
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    """Send an introduction message"""
+    bot.reply_to(message, "Welcome to memebot. To get a random meme, type /send_meme")
+
+
+@bot.message_handler(commands=['send_meme'])
+def send_meme(message):
+    """Send a meme to the user."""
+    response = requests.get('https://meme-api.herokuapp.com/gimme')
+    if not response.ok:
+        return
+    link = response.json()['url']
+    bot.reply_to(message, link)
 
 
 bot.infinity_polling()
